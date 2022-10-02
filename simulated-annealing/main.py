f9db3ce796b17c98581c56c99d1267d345b827ef
Nicolle <nicollepf@gmail.com>
2022-10-02 19:43:19 -0300

    Fim da implementação da função que gera a solução inicial

--- simulated-annealing/main.py
+++ simulated-annealing/main.py
@@ -1,6 +1,12 @@
 import input
 import structs
+import os
 
-instance = input.get_instances("instance_5_5.dat")
-solution = structs.SimulatedAnnealing(instance).first_solution()
-print(solution)
+current_dir = os.path.dirname(__file__)
+absolute_dir = os.path.join(current_dir, "../instances/")
+files = os.listdir(absolute_dir)
+
+for file in files:
+    instance = input.get_instances(file)
+    solution = structs.SimulatedAnnealing(instance).first_solution()
+    print(solution)
--- simulated-annealing/structs.py
+++ simulated-annealing/structs.py
@@ -1,16 +1,17 @@
 import random
+import copy
 
 class Subgraph():
-    def __init__(self, weight, available):
-        self.subgraph = {}
+    def __init__(self, subgraph, weight, available):
+        self.subgraph = subgraph
         self.weight = weight
-        self.available_vertices = []
+        self.available_vertices = available
 
     def __str__(self):
         return f"""
                 Vertices: {self.subgraph}
                 Total Weight: {self.weight}
-                Available Vertices: {self.available}
+                Available Vertices: {self.available_vertices}
                 """
 
 
@@ -51,27 +52,30 @@ class SimulatedAnnealing():
         # remaining: todos os vértices que passaram por available de todas iterações (todos vértices de todas iterações que não foram para explorados)
         # explored: todos os vértices já usados desde o inicio do loop
 
-        remaining = []
+        remaining = [x for x in range(0, self.instance.vertices_qt)]
         explored = []
         subgraphs_count = 0
 
         solution = Solution(True)
 
-        while subgraphs_count < self.instance.subgraphs_qt:
+        subgraph_qt = self.instance.subgraphs_qt
+        while subgraphs_count < subgraph_qt:
             subgraph = {}
 
-            i = random.randint(0, self.instance.vertices_qt)
+            i = random.randint(0, self.instance.vertices_qt - 1)
 
-            while (i in explored):
-                i = random.randint(0, self.instance.vertices_qt)
+            while (i in explored) and (len(explored) != self.instance.vertices_qt):
+                i = random.randint(0, self.instance.vertices_qt - 1)
 
-            subgraph[i] = self.instance.vertices_weights[i]
+            i_weight = self.instance.vertices_weights[i]
+            subgraph[i] = i_weight
             border = self.instance.vertices_edges[i]
 
             available = []
             explored.append(i)
+            remaining.remove(i)
 
-            weight = 0
+            weight = i_weight
 
             while True:
                 v = None
@@ -79,8 +83,9 @@ class SimulatedAnnealing():
                     if ((weight + self.instance.vertices_weights[j]) <= self.instance.max_weight) and (j not in explored):
                         v = j
                         break
-
-                available = list(set(available.append(border)))                     # remove duplicatas 
+                
+                available += border
+                available = list(set(available))                     # remove duplicatas 
                 
                 if v is None:
                     if weight < self.instance.min_weight:
@@ -95,46 +100,56 @@ class SimulatedAnnealing():
                     weight += weight_v
                     explored.append(v)
                     available.remove(v)  
-                    border = self.vertex_edges[v]                                   # atualiza border com vizinhos do vertice atual
+                    remaining.remove(v)
+                    border = self.instance.vertices_edges[v]         # atualiza border com vizinhos do vertice atual
                     subgraph[v] = weight_v
-
-            remaining = list(set(remaining.append(available)))
 
 
         # conferir se todos os vértices foram para algum subgrafo e tratar caso esse não tenha sido o caso
+        remaining = list(set(remaining))
         remaining_qt = len(remaining) 
+        remaining_aux = copy.deepcopy(remaining)
+
         if remaining_qt > 0:
             for vr in remaining:
-                vr_weight =  self.instance.vertices_weight[vr]
+                vr_weight =  self.instance.vertices_weights[vr]
                 for sg in solution.subgraphs:
-                    if (vr in sg.available) and ((sg.weight + vr_weight) <= self.instance.max_weight):
+                    if (vr in sg.available_vertices) and ((sg.weight + vr_weight) <= self.instance.max_weight):
                         sg.subgraph[vr] = vr_weight
                         sg.weight += vr_weight
-                        sg.available.remove(vr)
+                        sg.available_vertices.remove(vr)
+                        remaining_aux.remove(vr)
                         remaining_qt -= 1
+                        break
 
-        if remaining_qt > 0:
+        if remaining_qt == 0:
+            solution.feasible = True
+
+        else:
             solution.feasible = False
-            for vr in remaining:
-                vr_weight =  self.instance.vertices_weight[vr]
+            remaining = copy.deepcopy(remaining_aux)
+            len_sg = len(solution.subgraphs)
 
-                sg = 0
-                for idx, vr in enumerate(remaining):
-                    idx_sg = 0
-                    len_sg = len(solution.subgraphs)
+            sg = 0
+            for r, vr in enumerate(remaining_aux):
+                vr_weight =  self.instance.vertices_weights[vr]
+                idx_sg = 0
 
-                    if vr in solution.subgraphs[sg].available:
-                        idx_sg = sg
-                
-                    else:
-                        for i in range(0, len_sg):
-                            if vr in solution.subgraphs[i].available:
-                                idx_sg = i
+                if vr in solution.subgraphs[sg].available_vertices:
+                    idx_sg = sg
+            
+                else:
+                    for i in range(0, len_sg):
+                        if vr in solution.subgraphs[i].available_vertices:
+                            idx_sg = i
+                        else:
+                            return None        # Infactibilidade na conectividade do grafo daí
 
-                    solution.subgraphs[sg].subgraph[vr] = vr_weight
-                    solution.subgraphs[sg].weight += vr_weight
-                    solution.subgraphs[sg].available.remove(vr)
-                    sg = (sg + 1) % len_sg
+                solution.subgraphs[idx_sg].subgraph[vr] = vr_weight
+                solution.subgraphs[idx_sg].weight += vr_weight
+                solution.subgraphs[idx_sg].available_vertices.remove(vr)
+                remaining.remove(vr)
+                sg = (sg + 1) % len_sg
 
         return solution
 
@@ -151,8 +166,6 @@ class Instance:
         self.vertices_costs = costs 
 
     def __str__(self):
-
-        print("Enttrou em __str__")
         graph_infos = f"""
         Informações do Grafo:
         Quantidade de Vértices (n): {self.vertices_qt}
